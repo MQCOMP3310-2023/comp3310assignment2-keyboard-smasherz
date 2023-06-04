0073aa05df2d4a75b34c1e9f0af0a37d7f73c03f
Jeg-Marka <70504307+Jeg-Marka@users.noreply.github.com>
2023-06-04 13:49:08 +1000

    v5.0
    Included Batch file which allows devs to quickly install the required packages and versions

---
+++ packageInstaller.bat
@@ -0,0 +1,7 @@
+python -m pip install --upgrade pip
+python -m pip install flask==2.2.4
+python -m pip install pytz
+python -m pip install flask_login
+python -m pip install flask_security 
+python -m pip install email_validator
+python -m pip install flask_SQLAlchemy
